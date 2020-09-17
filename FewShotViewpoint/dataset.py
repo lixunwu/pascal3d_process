289ed621374affd7b7a589e225bab89a4bb4d6f4
wulixun <wu_lixun@foxmail.com>
2020-09-17 16:06:00 +0800

    用FewShotViewpoint的代码创建Pascal3D数据集类

---
+++ FewShotViewpoint/dataset.py
@@ -0,0 +1,188 @@
+import os
+import numpy as np
+from PIL import Image, ImageFilter
+import pandas as pd
+import pymesh
+
+import torch
+import torchvision.transforms as transforms
+import torch.utils.data as data
+
+imagenet_pca = {
+    'eigval': torch.tensor([0.2175, 0.0188, 0.0045]),
+    'eigvec': torch.tensor([[-0.5675, 0.7192, 0.4009],
+                            [-0.5808, -0.0045, -0.8140],
+                            [-0.5836, -0.6948, 0.4203]])}
+
+
+# 根据图像的bbox随机裁剪
+def random_crop(img, x, y, w, h):
+    left = max(0, x + int(w * np.random.uniform(low=-0.1, high=0.1)))
+    upper = max(0, y + int(h * np.random.uniform(low=-0.1, high=0.1)))
+    right = min(img.size[0], x + int(w * np.random.uniform(low=0.9, high=1.1)))
+    lower = min(img.size[1], x + int(h * np.random.uniform(low=0.9, high=1.1)))
+    img_crop = img.crop((left, upper, right, lower))
+    return img_crop
+
+
+# resize到dim*dim
+def resize_pad(img, dim):
+    w, h = img.size
+    # torchvison 只接收PIL图像,将最小边长缩放到给定的size,最大边长也按照等比例缩放
+    img = transforms.functional.resize(img, int(dim * (min(w, h) / max(w, h))))
+    left_pad = int(np.ceil((dim - img.size[0]) / 2))
+    right_pad = int(np.floor((dim - img.size[0]) / 2))
+    top_pad = int(np.ceil((dim - img.size[1]) / 2))
+    bottom_pad = int(np.floor((dim - img.size[1]) / 2))
+    img = transforms.functional.pad(img, (left_pad, top_pad, right_pad, bottom_pad))
+    return img
+
+
+# Lighting noise transform
+class TransLightning(object):
+    def __init__(self, alphastd, eigval, eigvec):
+        self.alphastd = alphastd
+        self.eigval = eigval
+        self.eigvec = eigvec
+
+    def __call__(self, img):
+        if self.alphastd == 0:
+            return img
+
+        alpha = img.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(img).clone() \
+            .mul(alpha.view(1, 3).expand(3, 3)) \
+            .mul(self.eigval.view(1, 3).expand(3, 3)) \
+            .sum(1).squeeze()
+        return img.add(rgb.view(3, 1, 1).expand_as(img))
+
+
+def read_pointcloud(model_path, point_num):
+    raw_data = pymesh.load_mesh(model_path).vertices
+    point_subset = np.random.choice(raw_data.shape[0], point_num, replace=False)
+    point_cloud = raw_data[point_subset]
+    point_cloud = torch.from_numpy(point_cloud.transpose()).float()
+
+    # 归一化到[0,1]
+    point_cloud = point_cloud - torch.min(point_cloud)
+    point_cloud = point_cloud / torch.max(point_cloud)
+    return point_cloud
+
+
+class Pascal3D(data.Dataset):
+    def __init__(self, root_dir, annotation_file, input_dim=224, point_num=2500,
+                 train=True, keypoint=True, novel=True, cls_choice=None, shot=None):
+        self.train = train
+        self.root_dir = root_dir
+        self.input_dim = input_dim
+        self.point_num = point_num
+
+        # 从annotation_file(Pascal3D.txt)中读取数据
+        frame = pd.read_csv(os.path.join(root_dir, annotation_file))
+        # 将ele=90和difficult的数据清除
+        frame = frame[frame.elevation != 90]
+        frame = frame[frame.difficult == 0]
+
+        if train:
+            frame = frame[frame.set == 'train']
+
+        else:
+            # 只使用没有遮挡和截断且有关键点的数据来进行验证
+            frame = frame[frame.set == 'val']
+            frame = frame[frame.truncated == 0]
+            frame = frame[frame.occluded == 0]
+            frame = frame[frame.has_keypoints == 1]
+
+        # 在训练数据中 如果keypoint==True 则只选择有keypoint的数据进行训练
+        if train and keypoint:
+            frame = frame[frame.has_keypoints == 1]
+
+        self.annotation_frame = frame
+        self.im_augmentation = transforms.Compose([
+            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            TransLightning(0.1, imagenet_pca['eigval'], imagenet_pca['eigvec'])
+        ])
+        self.im_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        self.render_transform = transforms.ToTensor()
+        pass
+
+    def __len__(self):
+        return len(self.annotation_frame)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root_dir, self.annotation_frame.iloc[idx]['im_path'])
+        cls = self.annotation_frame.iloc[idx]['cat']
+
+        # 随机选择一个shape
+        cls_index = np.unique(self.annotation_frame[self.annotation_frame.cat == cls].cad_index)
+        cad_index = np.random.choice(cls_index)
+
+        # bbox
+        left = self.annotation_frame.iloc[idx]['left']
+        upper = self.annotation_frame.iloc[idx]['upper']
+        right = self.annotation_frame.iloc[idx]['right']
+        lower = self.annotation_frame.iloc[idx]['lower']
+
+        # viewpoint
+        viewpoint = self.annotation_frame.iloc[idx, 9:12].values
+
+        # 读取图像CHW RGB
+        img = Image.open(img_path).convert('RGB')
+
+        # 如果数据集是用来训练的,则做一些增强操作
+        if self.train:
+            # 高斯模糊
+            if min(right - left, lower - upper) > 224 and np.random.random() < 0.3:
+                img = img.filter(ImageFilter.GaussianBlur(3))
+
+            # 使用bbox随机裁剪
+            img = random_crop(img=img, x=left, y=upper, w=right - left, h=lower - upper)
+
+            # 水平翻转
+            if np.random.random() > 0.5:
+                img = img.transpose(Image.FLIP_LEFT_RIGHT)
+                viewpoint[0] = 360 - viewpoint[0]
+                viewpoint[2] = -viewpoint[2]
+
+            # 随机旋转
+            if np.random.random() > 0.5:
+                r = max(-60, min(60, np.random.randn() * 30))
+                img.rotate(r)
+                viewpoint[2] = viewpoint[2] + r
+                viewpoint[2] += 360 if viewpoint[2] < -180 else (-360 if viewpoint[2] > 180 else 0)
+
+            # 将图片resize到固定大小(224*224)
+            img = resize_pad(img, self.input_dim)
+            img = self.im_augmentation(img)
+
+        # 如果不是用来训练的,则不进行数据增强操作,只进行crop和resize
+        else:
+            img = img.crop((left, upper, right, lower))
+            img = resize_pad(img, self.input_dim)
+            img = self.im_transform(img)
+
+        # 将viewpoint 转换为[0,360) 没有必要?
+        # viewpoint[0] = (360. - viewpoint[0]) % 360.
+        # viewpoint[1] = viewpoint[1] + 90.
+        # viewpoint[2] = (viewpoint[2] + 180.) % 360.
+        viewpoint = viewpoint.astype('int')
+
+        # load point_clouds
+        point_cloud_path = os.path.join(self.root_dir, 'Pointclouds', cls, '{:02d}'.format(
+            cad_index), 'compressed.ply')
+        point_cloud = read_pointcloud(point_cloud_path, self.point_num)
+
+        return {'img': img, 'point_cloud': point_cloud, 'label': viewpoint, 'class': cls}
+
+
+if __name__ == '__main__':
+    pascal3d = Pascal3D(root_dir='/opt/WLX/dataset/PASCAL3D+_release1.1',
+                        annotation_file='Pascal3D.txt')
+    data = pascal3d[0]
+    print('end')
--- FewShotViewpoint/template.py
+++ FewShotViewpoint/template.py
@@ -1,93 +1,207 @@
-# 这个文件是FewShotViewpoint的源文件
 import os
-import scipy.io as sio
 import numpy as np
-from PIL import Image
-from tqdm import trange
-import cv2
+from PIL import Image, ImageFilter
+import pandas as pd
+import pymesh
 
-cats = ['aeroplane', 'bicycle', 'boat', 'bottle', 'bus', 'car', 'chair', 'diningtable', 'motorbike', 'sofa', 'train',
-        'tvmonitor']
-databases = ['imagenet', 'pascal']
-subsets = ['train', 'val']
+import torch
+import torchvision.transforms as transforms
+import torch.utils.data as data
 
-annotation_file = 'Pascal3D.txt'
-if not os.path.exists(annotation_file):
-    with open(annotation_file, 'a') as f:
-        f.write(
-            'source,set,cat,im_name,object,cad_index,truncated,occluded,difficult,azimuth,elevation,inplane_rotation,left,upper,right,lower,has_keypoints,distance,im_path\n')
 
-for subset in subsets:
-    for database in databases:
-        if subset == 'val' and database == 'imagenet':
-            continue
+# Lighting noise transform
+class TransLightning(object):
+    def __init__(self, alphastd, eigval, eigvec):
+        self.alphastd = alphastd
+        self.eigval = eigval
+        self.eigvec = eigvec
 
-        for cat in cats:
-            # 无论是'imagenet'还是'pascal',图片数据和标注数据都在'./Images'和'./Annotations'下
-            image_dir = os.path.join('Images', cat + '_' + database)
-            annotation_dir = os.path.join('Annotations', cat + '_' + database)
+    def __call__(self, img):
+        if self.alphastd == 0:
+            return img
 
-            # read the validation image list
-            # 数据集val数据列表
-            if database == 'imagenet':
-                val_list_file = os.path.join('Image_sets', '{}_imagenet_val.txt'.format(cat))
+        alpha = img.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(img).clone() \
+            .mul(alpha.view(1, 3).expand(3, 3)) \
+            .mul(self.eigval.view(1, 3).expand(3, 3)) \
+            .sum(1).squeeze()
+        return img.add(rgb.view(3, 1, 1).expand_as(img))
+
+
+# ImageNet statistics
+imagenet_pca = {
+    'eigval': torch.Tensor([0.2175, 0.0188, 0.0045]),
+    'eigvec': torch.Tensor([[-0.5675, 0.7192, 0.4009],
+                            [-0.5808, -0.0045, -0.8140],
+                            [-0.5836, -0.6948, 0.4203],
+                            ])
+}
+
+# Define normalization and random disturb for input image
+disturb = TransLightning(0.1, imagenet_pca['eigval'], imagenet_pca['eigvec'])
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+
+def random_crop(im, x, y, w, h):
+    left = max(0, x + int(np.random.uniform(-0.1, 0.1) * w))
+    upper = max(0, y + int(np.random.uniform(-0.1, 0.1) * h))
+    right = min(im.size[0], x + int(np.random.uniform(0.9, 1.1) * w))
+    lower = min(im.size[1], y + int(np.random.uniform(0.9, 1.1) * h))
+    im_crop = im.crop((left, upper, right, lower))
+    return im_crop
+
+
+def resize_pad(im, dim):
+    w, h = im.size
+    im = transforms.functional.resize(im, int(dim * min(w, h) / max(w, h)))
+    left = int(np.ceil((dim - im.size[0]) / 2))
+    right = int(np.floor((dim - im.size[0]) / 2))
+    top = int(np.ceil((dim - im.size[1]) / 2))
+    bottom = int(np.floor((dim - im.size[1]) / 2))
+    im = transforms.functional.pad(im, (left, top, right, bottom))
+    return im
+
+
+def read_pointcloud(model_path, point_num):
+    # read in original point cloud
+    point_cloud_raw = pymesh.load_mesh(model_path).vertices
+
+    # randomly select a fix number of points on the surface
+    point_subset = np.random.choice(point_cloud_raw.shape[0], point_num, replace=False)
+    point_cloud = point_cloud_raw[point_subset]
+    point_cloud = torch.from_numpy(point_cloud.transpose()).float()
+
+    # normalize the point cloud into [0, 1]
+    point_cloud = point_cloud - torch.min(point_cloud)
+    point_cloud = point_cloud / torch.max(point_cloud)
+    
+    return point_cloud
+
+
+# ================================================= #
+# Datasets used for training
+# ================================================= #
+class Pascal3D(data.Dataset):
+    def __init__(self, root_dir, annotation_file, input_dim=224, point_num=2500, train=True,
+                 keypoint=True, novel=True, cls_choice=None, shot=None):
+        self.train = train
+        self.root_dir = root_dir
+        self.input_dim = input_dim
+        self.point_num = point_num
+
+        # load the data frame for annotations
+        frame = pd.read_csv(os.path.join(root_dir, annotation_file))
+        frame = frame[frame.elevation != 90]
+        frame = frame[frame.difficult == 0]
+        if annotation_file == 'ObjectNet3D.txt':
+            frame.azimuth = (360. + frame.azimuth) % 360
+
+        # evaluation only on non-occluded and non-truncated objects with keypoint annotations as MetaView/StarMap
+        if train:
+            frame = frame[frame.set == 'train']
+        else:
+            frame = frame[frame.set == 'val']
+            frame = frame[frame.truncated == 0]
+            frame = frame[frame.occluded == 0]
+            frame = frame[frame.has_keypoints == 1]
+
+        # we exclude training samples without keypoint annotations for a fair comparison with MetaView
+        if train and keypoint:
+            frame = frame[frame.has_keypoints == 1]
+
+        # exclude novel classes for training and include it for testing
+        if cls_choice is not None:
+            if train:
+                frame = frame[~frame.cat.isin(cls_choice)] if novel else frame
             else:
-                val_list_file = os.path.join('PASCAL', 'VOCdevkit', 'VOC2012', 'ImageSets', 'Main',
-                                             '{}_val.txt'.format(cat))
-            with open(val_list_file, 'r') as f:
-                val_list = [line.rstrip('\n') for line in f]
+                frame = frame[frame.cat.isin(cls_choice)]
 
-            # remove the classification label exiting in pascal image list
-            if database == 'pascal':
-                val_list = [str(line.split()[0]) for line in val_list]
+        # sample K-shot training data
+        if train and shot is not None:
+            categories = np.unique(frame.cat)
+            fewshot_frame = []
+            for cat in categories:
+                fewshot_frame.append(frame[frame.cat == cat].sample(n=shot))
+            frame = pd.concat(fewshot_frame)
+        self.annotation_frame = frame
 
-            # read image names
-            im_files = [name for name in os.listdir(image_dir)]
-            im_files.sort()
+        # define data augmentation and preprocessing for RGB images in training
+        self.im_augmentation = transforms.Compose([
+            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5),
+            transforms.ToTensor(), normalize, disturb])
 
-            # read annotations and transform into csv files
-            for n in trange(len(im_files),
-                            desc='reading category {} for {} set of {} database'.format(cat, subset, database)):
-                im_file = im_files[n]
-                im_name = str(im_file.split('.')[0])
+        # define data preprocessing for RGB images in validation
+        self.im_transform = transforms.Compose([transforms.ToTensor(), normalize])
 
-                if subset == 'train' and database == 'pascal' and (im_name in val_list):
-                    continue
-                if subset == 'val' and not (im_name in val_list):
-                    continue
+        # define data preprocessing for rendered multi view images
+        self.render_transform = transforms.ToTensor()
+        if input_dim != 224:
+            self.render_transform = transforms.Compose([transforms.Resize(input_dim), transforms.ToTensor()])
 
-                record = (sio.loadmat(os.path.join(annotation_dir, im_name + '.mat')))['record'][0][0]
-                objects = record['objects'][0]
-                for i in range(objects.shape[0]):
-                    object_cls = objects[i]['class'].item()
-                    if object_cls != cat:
-                        continue
-                    truncated = str(objects[i]['truncated'].item())
-                    occluded = str(objects[i]['occluded'].item())
-                    difficult = str(objects[i]['difficult'].item())
-                    cad_index = str(objects[i]['cad_index'].item())
-                    viewpoint = objects[i]['viewpoint'][0][0]
-                    try:
-                        azimuth = str(viewpoint['azimuth'].item())
-                        elevation = str(viewpoint['elevation'].item())
-                    except:
-                        azimuth = str(viewpoint['azimuth_coarse'].item())
-                        elevation = str(viewpoint['elevation_coarse'].item())
-                    inplane_rotation = str(viewpoint['theta'].item())
-                    if azimuth == '0' and elevation == '0' and inplane_rotation == '0':
-                        continue
-                    distance = str(viewpoint['distance'].item())
-                    try:
-                        anchors = objects[i]['anchors'][0][0]
-                        has_keypoint = str(1) if len(anchors) > 0 else str(0)
-                    except:
-                        has_keypoint = str(0)
-                    bbox = tuple((objects[i]['bbox'][0]).astype('int'))
-                    left, upper, right, lower = str(bbox[0]), str(bbox[1]), str(bbox[2]), str(bbox[3])
+    def __len__(self):
+        return len(self.annotation_frame)
 
-                    with open(annotation_file, 'a') as f:
-                        f.write(
-                            database + ',' + subset + ',' + cat + ',' + im_name + ',' + str(i) + ',' + cad_index + ',' +
-                            truncated + ',' + occluded + ',' + difficult + ',' + azimuth + ',' + elevation + ',' + inplane_rotation + ',' +
-                            left + ',' + upper + ',' + right + ',' + lower + ',' + has_keypoint + ',' + distance + ',' + os.path.join(
-                                image_dir, im_file) + '\n')
+    def __getitem__(self, idx):
+        # get image path and category
+        img_path = os.path.join(self.root_dir, self.annotation_frame.iloc[idx, -1])
+        cls = self.annotation_frame.iloc[idx]['cat']
+
+        # randomly choose a shape or an exemplar shape
+        cls_index = np.unique(self.annotation_frame[self.annotation_frame.cat == cls].cad_index)
+        cad_index = np.random.choice(cls_index)
+
+        left = self.annotation_frame.iloc[idx]['left']
+        upper = self.annotation_frame.iloc[idx]['upper']
+        right = self.annotation_frame.iloc[idx]['right']
+        lower = self.annotation_frame.iloc[idx]['lower']
+
+        # use continue viewpoint annotation
+        label = self.annotation_frame.iloc[idx, 9:12].values
+
+        # load real images in a Tensor of size C*H*W
+        im = Image.open(img_path).convert('RGB')
+
+        if self.train:
+            # Gaussian blur
+            if min(right - left, lower - upper) > 224 and np.random.random() < 0.3:
+                im = im.filter(ImageFilter.GaussianBlur(3))
+
+            # crop the original image with 2D bounding box jittering
+            im = random_crop(im, left, upper, right - left, lower - upper)
+
+            # Horizontal flip
+            if np.random.random() > 0.5:
+                im = im.transpose(Image.FLIP_LEFT_RIGHT)
+                label[0] = 360 - label[0]
+                label[2] = -label[2]
+
+            # Random rotation
+            if np.random.random() > 0.5:
+                r = max(-60, min(60, np.random.randn() * 30))
+                im = im.rotate(r)
+                label[2] = label[2] + r
+                label[2] += 360 if label[2] < -180 else (-360 if label[2] > 180 else 0)
+
+            # pad it to the desired size
+            im = resize_pad(im, self.input_dim)
+            im = self.im_augmentation(im)
+        else:
+            # crop the ground truth bounding box and pad it to the desired size
+            im = im.crop((left, upper, right, lower))
+            im = resize_pad(im, self.input_dim)
+            im = self.im_transform(im)
+
+        # transform the ground-truth angle values into [0, 360)
+        label[0] = (360. - label[0]) % 360.
+        label[1] = label[1] + 90.
+        label[2] = (label[2] + 180.) % 360.
+        label = label.astype('int')
+
+        # load point_clouds
+        pointcloud_path = os.path.join(self.root_dir, 'Pointclouds', cls, '{:02d}'.format(cad_index), 'compressed.ply')
+        point_cloud = read_pointcloud(pointcloud_path, self.point_num)
+
+        if self.train:
+            return im, point_cloud, label, cls
+        else:
+            return im, point_cloud, label
